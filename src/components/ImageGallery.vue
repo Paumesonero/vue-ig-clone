3b80c67f99cd27dcecff919af62a736023190f1a
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2023-05-19 19:07:01 +0200

    ui done

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,6 +1,22 @@
-<script setup></script>
-<template>
-    <div>
+<script setup>
 
+const { posts } = defineProps(['posts'])
+</script>
+<template>
+    <div class="gallery-container">
+        <img v-for="el in posts" :key="el.id" :src="el.img" alt="elantris">
     </div>
 </template>
+
+<style scoped>
+.gallery-container {
+    display: flex;
+    justify-content: left;
+    flex-wrap: wrap;
+}
+
+img {
+    margin: 5px;
+    width: 200px;
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Container from './Container.vue';
+import ImageGallery from './ImageGallery.vue';
 import UserBar from './UserBar.vue';
 
 </script>
@@ -7,12 +8,20 @@ import UserBar from './UserBar.vue';
     <Container>
         <div class="profile-container">
             <UserBar userName="raoden" :userInfo="{
-                    posts: 4,
-                    followers: 100,
-                    following: 233
-                }" />
-            <h2>Gallery</h2>
+                posts: 4,
+                followers: 100,
+                following: 233
+            }" />
+            <ImageGallery :posts="[{
+                id: 1,
+                img: 'https://www.17thshard.com/forum/uploads/monthly_2020_10/IMG_20201003_154853.JPG.dacbb3fcbb3e2933e4bc739739b47536.JPG'
+            },
+            {
+                id: 2,
+                img: 'https://www.brandonsanderson.com/wp-content/uploads/2019/11/00_elantris_us.jpg'
+            }]" />
         </div>
+
     </Container>
 </template>
 <style scoped>
